0b288a62f164b3bc834c3c4f7dd036e92bc93fd3
al1h3n <alikhanalquat+work@gmail.com>
2026-03-30 20:20:45 +0500

    Virt fix 1

--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -27,7 +27,9 @@
 
   systemd.services.libvirtd.environment = {
     __EGL_VENDOR_LIBRARY_DIRS = "/run/opengl-driver/share/glvnd/egl_vendor.d";
-    GBM_BACKEND = "nvidia-drm";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    # GBM_BACKEND = "nvidia-drm";
+    EGL_PLATFORM = "device";
+    LIBGL_ALWAYS_SOFTWARE = "0";
   };
 }
