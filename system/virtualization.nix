dd896f832d9789df10f78cc5c09b9d7a55d9fe25
al1h3n <alikhanalquat+work@gmail.com>
2026-03-30 20:33:26 +0500

    Virt fix

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -7,13 +7,20 @@
         "/dev/null", "/dev/full", "/dev/zero",
         "/dev/random", "/dev/urandom",
         "/dev/ptmx", "/dev/kvm",
+
         "/dev/dri/card0",
         "/dev/dri/card1",
         "/dev/dri/renderD128",
-        "/dev/dri/renderD129"
+        "/dev/dri/renderD129",
+
+        "/dev/nvidia0",
+        "/dev/nvidiactl",
+        "/dev/nvidia-modeset",
+        "/dev/nvidia-uvm",
+        "/dev/nvidia-uvm-tools"
       ]
+
       nvram = []
-      '';
+    '';
     };
-  };
 }
