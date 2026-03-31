7d93ae1212ad60a7f70caa3ca8e12b20c3ca6b5c
al1h3n <alikhanalquat+work@gmail.com>
2026-03-31 19:35:59 +0500

    Fix

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -24,4 +24,15 @@
     '';
     };
   };
+  systemd.services.libvirtd-network-default = {
+    description = "libvirt default network autostart - MolniOS";
+    after = [ "libvirtd.service" ];
+    requires = [ "libvirtd.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      ${pkgs.libvirt}/bin/virsh net-autostart default
+      ${pkgs.libvirt}/bin/virsh net-start default || true
+    '';
+  };
 }
