9d2d014ed047b995967cf5efce170c41ac095499
al1h3n <alikhanalquat+work@gmail.com>
2026-03-31 19:09:15 +0500

    Fix

--- home/dots/ui.nix
+++ home/dots/ui.nix
@@ -37,7 +37,7 @@ in {
   };
 
   home = {
-    packages = [ pkgs.qt6ct icons.mactahoe pkgs.kdePackages.breeze pkgs.kdePackages.breeze-gtk ];
+    packages = [ pkgs.qt6Packages.qt6ct icons.mactahoe pkgs.kdePackages.breeze pkgs.kdePackages.breeze-gtk ];
     sessionVariables = {
       QT_QPA_PLATFORMTHEME = "breeze";
       # QT_STYLE_OVERRIDE = "breeze-dark";
