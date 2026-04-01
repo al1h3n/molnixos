6342477e1a2ff5eb7b0308a08523b070252ea017
al1h3n <alikhanalquat+work@gmail.com>
2026-04-01 14:10:11 +0500

    Thunar updates

---
+++ home/dots/thunar.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: {
+  xfconf.settings = {
+    thunar = {
+      "last-show-hidden" = true;  # Thunar file manager window
+    };
+  };
+  dconf.settings = {
+    "org/gtk/settings/file-chooser" = {
+      show-hidden = true;  # GTK open/save dialogs in all apps
+    };
+  };
+}
