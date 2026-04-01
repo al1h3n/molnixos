00f813b6f0a0f16fa260b29497a605c9ca9d036f
al1h3n <alikhanalquat+work@gmail.com>
2026-04-01 21:58:48 +0500

    Beta fix

--- home/dots/peazip.nix
+++ home/dots/peazip.nix
@@ -1,7 +1,11 @@
-{ pkgs, config, variables, ... }: {
+{ variables, ... }: {
   xdg.configFile."dark".text = "";
-  xdg.configFile."peazip/conf.txt"= {
-    source = variables.peazip;
-    force = true;
-  };
+  # xdg.configFile."peazip/conf.txt"= {
+  #   source = variables.peazip;
+  #   force = true;
+  # };
+
+  xdg.configFile."peazip/conf.txt".text = builtins.replaceStrings
+    [ "~/.local/share/molnios" ][ variables.lshared ]
+    (builtins.readFile variables.peazip);
 }
