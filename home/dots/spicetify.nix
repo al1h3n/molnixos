294391e9fd6df447bfe93b5baac96cfa9e181142
al1h3n <alikhanalquat+work@gmail.com>
2026-04-17 17:36:33 +0500

    Spotify changes

--- home/dots/spicetify.nix
+++ home/dots/spicetify.nix
@@ -7,14 +7,34 @@ in {
   programs.spicetify = {
     enable = true;
 
-    # Optional: pick a theme
-    theme = spicePkgs.themes.catppuccin;
+    # Marketplace.
+    enabledCustomApps = with spicePkgs.apps; [ marketplace ];
+
+    # Theme.
+    theme =
+    # Or something like spicePkgs.themes.catppuccin
     colorScheme = "mocha";
 
     # Optional: add extensions
     enabledExtensions = with spicePkgs.extensions; [
       adblock
-      shuffle
+      shuffle # Proper shuffle with zero bias.
+      autoSkipVideo
+      hidePodcasts
+      keyboardShortcut
+      volumePercentage
+      loopyLoop # Select a part to loop.
+      trashbin # Remove artists from playing.
+      betterGenres # Song genre.
+
+      aiBandBlocker # Skip AI slop.
+      powerBar # macOS bar for search
+      copyToClipboard # Copy song name.
+      spicyLyrics # Better lyrics.
+      copyLyrics
+      history # History of playing.
+      sessionStats
+      # focusMode
     ];
   };
 }
