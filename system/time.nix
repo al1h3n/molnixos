2ba232f3130f3fbeb8d8dfe6fb7b77288fd6fd1d
al1h3n <alikhanalquat+work@gmail.com>
2026-04-18 17:20:52 +0500

    Fix

--- system/time.nix
+++ system/time.nix
@@ -4,14 +4,11 @@
 
   services.chrony = {
     enable = true;
-    servers = [
-      "pool.ntp.org"
-      "time.cloudflare.com"
-    ];
+    servers = [ "pool.ntp.org" "time.cloudflare.com" ];
     extraConfig = ''
-      rtconutc off        # RTC is in local time, not UTC
-      makestep 1.0 3      # Step clock immediately if off by >1s (first 3 updates)
-      rtcsync             # Sync RTC from system clock periodically
+      rtconutc
+      makestep 1.0 3
+      rtcsync
     '';
   };
 }
