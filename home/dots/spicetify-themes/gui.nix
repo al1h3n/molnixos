d61ed9ad9f8aa05639fb9fe2f4a1dce70a11ee8b
al1h3n <alikhanalquat+work@gmail.com>
2026-04-18 19:29:56 +0500

    Fix

--- home/dots/spicetify-themes/gui.nix
+++ home/dots/spicetify-themes/gui.nix
@@ -4,9 +4,6 @@ let
   spicePkgs = inputs.spicetify.legacyPackages.${pkgs.stdenv.hostPlatform.system};
 in {
   programs.spicetify = {
-    enabledExtensions = with spicePkgs.extensions; [
-      powerBar # macOS bar for search
-    ];
     enabledSnippets = with spicePkgs.snippets; [ sonicDancing ];
   };
 }
