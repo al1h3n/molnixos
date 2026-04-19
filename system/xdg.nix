d29b69f0ab8da0866eedbb5e7963038a823889d3
al1h3n <alikhanalquat+work@gmail.com>
2026-04-19 11:59:47 +0500

    Fix

--- system/xdg.nix
+++ system/xdg.nix
@@ -4,7 +4,7 @@
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     config = {
       common.default = [ "gtk" ]; # Or *
-      "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+      # "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
       hyprland = {
         default = [ "hyprland" "gtk" ];
         "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
