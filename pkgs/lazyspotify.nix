94c96702a28877006cd44b24069052cb5e029b37
al1h3n <alikhanalquat+work@gmail.com>
2026-04-19 11:45:51 +0500

    Fix

--- pkgs/lazyspotify.nix
+++ pkgs/lazyspotify.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "lazyspotify";
   version = "unstable";
   inherit src;
-  vendorHash = "sha256-Axdt3/3ZOZY9Z5VUI6Wh77oIREOO26ODMyEgtscTmn8=";
+  vendorHash = "sha256-Axdt3/3ZOZY9Z5VUI6Wh77oIREOO26ODMyEgtscTmn8="; # Use lib.fakeHash if build is failed (does not affect version).
   subPackages = [ "cmd/lazyspotify" ];
   meta = with lib; {
     description = "Terminal Spotify client";
