f6d1c8beb8b3e321e91d2f945b8b2f693ae4f2a3
al1h3n <alikhanalquat+work@gmail.com>
2026-04-19 11:42:19 +0500

    Fix

--- pkgs/lazyspotify.nix
+++ pkgs/lazyspotify.nix
@@ -1,15 +1,10 @@
 { lib, buildGoModule, src }:
-
 buildGoModule {
   pname = "lazyspotify";
   version = "unstable";
-
-  inherit src; # comes from flake input, hash is in flake.lock
-
-  vendorHash = "sha256-XXXX"; # only changes when go.mod/go.sum changes
-
+  inherit src;
+  vendorHash = lib.fakeHash; # ← changed from "sha256-XXXX"
   subPackages = [ "cmd/lazyspotify" ];
-
   meta = with lib; {
     description = "Terminal Spotify client";
     homepage = "https://github.com/dubeyKartikay/lazyspotify";
