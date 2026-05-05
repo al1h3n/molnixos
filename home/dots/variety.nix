e8ec839a5e88cab70b5bee87e92a65ace5effcaf
al1h3n <al1h3n@molni.com>
2026-05-05 19:54:57 +0500

    Fix

--- home/dots/variety.nix
+++ home/dots/variety.nix
@@ -1,13 +1,22 @@
-{ pkgs, variables, ... }: {
+{ pkgs, lib, variables, ... }: {
   home.packages = [ pkgs.variety ];
-  xdg.configFile = {
-    "variety/scripts/set_wallpaper.sh" = {
-      executable = true;
-      text = ''
-        # Variety passes the wallpaper path as $1
-        ${pkgs.waypaper}/bin/waypaper --wallpaper "$1" --backend awww --restore
-      '';
-    };
+  # xdg.configFile = {
+  #   "variety/scripts/set_wallpaper.sh" = {
+  #     executable = true;
+  #     text = ''
+  #       # Variety passes the wallpaper path as $1
+  #       ${pkgs.waypaper}/bin/waypaper --wallpaper "$1" --backend awww --restore
+  #     '';
+  #   };
+
+   home.activation.varietySetterScript = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      mkdir -p ~/.config/variety/scripts
+      cat > ~/.config/variety/scripts/set_wallpaper.sh << 'EOF'
+  #!/bin/sh
+  ${pkgs.waypaper}/bin/waypaper --wallpaper "$1" --backend swww --restore
+  EOF
+      chmod +x ~/.config/variety/scripts/set_wallpaper.sh
+    '';
 
     "variety/variety.conf" = {
       text = ''
