f048f7c1aa8e5c2bf13ebe074b7ff4455fb87cda
al1h3n <al1h3n@molni.com>
2026-05-17 01:13:48 +0500

    Fix

--- home/dots/fonts.nix
+++ home/dots/fonts.nix
@@ -2,12 +2,6 @@
 let
   applefonts = inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system};
 in {
-  # home.packages = [
-  #   applefonts.sf-pro
-  #   applefonts.sf-pro-nerd
-  #   applefonts.sf-mono-nerd
-  # ];
-
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
