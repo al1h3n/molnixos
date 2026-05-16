fd01bfb1274d3a0f05c1c61ce33fef9b21f9a432
al1h3n <al1h3n@molni.com>
2026-05-17 01:13:38 +0500

    Font change

--- home/dots/fonts.nix
+++ home/dots/fonts.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }: 
+{ pkgs, inputs, ... }:
 let
   applefonts = inputs.apple-fonts.packages.${pkgs.stdenv.hostPlatform.system};
 in {
@@ -11,9 +11,9 @@ in {
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      sansSerif = [ "JetBrainsMono Nerd Font" "SF Mono" ];
-      serif     = [ "JetBrainsMono Nerd Font" "SF Pro" ];
-      monospace = [ "JetBrainsMono Nerd Font" "SF Pro" ];
+      sansSerif = [ "JetBrainsMono Nerd Font Propo" "SF Mono" ];
+      serif     = [ "JetBrainsMono Nerd Font Propo" "SF Pro" ];
+      monospace = [ "JetBrainsMono Nerd Font Propo" "SF Pro" ];
     };
   };
 }
