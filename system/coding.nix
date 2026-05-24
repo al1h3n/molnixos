f03b13ad3157daa77c0606df91cf22b3a3c7f4f6
al1h3n <al1h3n@molni.com>
2026-05-24 20:47:14 +0500

    Fix

--- system/coding.nix
+++ system/coding.nix
@@ -2,7 +2,7 @@
   environment.systemPackages = with pkgs; [
     gnumake gcc
     python3
-    (pipx.overrideAttrs (old: { doCheck = false; })) # pipx temporary fix
+    (pipx.overrideAttrs (_: { doCheck = false; })) # pipx temporary fix
     temurin-bin-8 temurin-bin-21 temurin-bin-25
     cargo nodejs-slim_22
   ];
