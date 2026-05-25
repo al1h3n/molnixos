0c4e2031a459fdbcbc0a99c169b8563b29355acb
al1h3n <al1h3n@molni.com>
2026-05-25 22:50:43 +0500

    Fix

--- system/git.nix
+++ system/git.nix
@@ -1,11 +1,11 @@
 { ... }:{
   nix = {
     settings = {
+      experimental-features = [ "configurable-impure-env" ];
       # This globally forces Git to use blobless partial cloning AND shallow depth=1
       impure-env = [
         "GIT_CONFIG_PARAMETERS"
       ];
-      shallow-clone = true;
     };
   };
   environment.variables = { GIT_CONFIG_PARAMETERS = "'git.cloneConfig=filter=blob:none' 'git.cloneConfig=depth=1'"; };
