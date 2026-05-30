a8aa6894f9e21bd0ecd6178552eafcc3258d95f3
al1h3n <al1h3n@molni.com>
2026-05-30 11:48:50 +0500

    Disabled more bloatware from Plasma

--- pkgs/plasma.nix
+++ pkgs/plasma.nix
@@ -1,12 +1,21 @@
 { pkgs, ... }: {
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    ark # File archiver
     elisa # Music player
+    dolphin # File manager
     konversation # IRC client
     kpat # Solitaire
     kmahjongg
+    kwalletmanager
+    kate # Editor
+    konsole
+    qrca # Code scanner
+    gwenview # Image viewer
+    khelpcenter
     ksudoku
     ktorrent
-    # oxygen # Uncomment to remove oxygen style
+    oxygen # Uncomment to remove oxygen style
+    okular
   ];
 }
