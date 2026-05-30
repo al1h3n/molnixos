17c0d7cf019dc1ae8f2c029bc2b65a2a084bec99
al1h3n <al1h3n@molni.com>
2026-05-30 13:58:37 +0500

    Fix

--- home/dots/ui.nix
+++ home/dots/ui.nix
@@ -40,8 +40,8 @@ in {
   systemd.user.sessionVariables = {
     GTK_THEME = variables.theme_gtk;
     QT_QPA_PLATFORMTHEME = lib.mkForce "qt6ct"; # Use lib.mkForce if you have errors.
-    QT_QPA_PLATFORM = "wayland;xcb";
     QT5_QPA_PLATFORMTHEME = "qt5ct";
+    QT_QPA_PLATFORM = "wayland;xcb";
   };
 
   # Packages.
