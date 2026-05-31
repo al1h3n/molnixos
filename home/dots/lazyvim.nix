ddb1c8cabb57aba61549e02e823518e7cc83ae4e
al1h3n <al1h3n@molni.com>
2026-05-31 16:02:59 +0500

    Mini changes

--- home/dots/lazyvim.nix
+++ home/dots/lazyvim.nix
@@ -18,7 +18,7 @@ in {
     enable = true;
     extras = allLangs;
   };
-  xdg.configFile."nvim/lua/plugins/colorscheme.lua".source = sym "theme.lua";
+  xdg.configFile."nvim/lua/plugins/theme.lua".source = sym "theme.lua";
   xdg.configFile."nvim/lua/plugins/dashboard.lua".source = sym "dashboard.lua";
   xdg.configFile."nvim/lua/config/autocmds.lua".source = sym "autocmds.lua";
 }
