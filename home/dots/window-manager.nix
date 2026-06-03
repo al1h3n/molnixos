f0baf40dbf7eb5f8fe7e21db29faf8d8ed9f1506
al1h3n <al1h3n@molni.com>
2026-06-03 15:54:40 +0500

    Fix

--- home/dots/window-manager.nix
+++ home/dots/window-manager.nix
@@ -21,7 +21,7 @@
       (tesseract5.override { enableLanguages = [ "eng" "rus" "chi_sim" ]; })
 
       # Wallpapers.
-      awww mpvpaper waypaper pywal16 matugen
+      awww mpvpaper waypaper pywal16 wallust matugen
       # Screenshots and recorders.
       grim slurp wf-recorder satty hyprshot
 
