9b026ac7ad99cee81f2bdaac7b2b2910dda80622
al1h3n <al1h3n@molni.com>
2026-06-12 17:23:45 +0500

    Niri fix

--- pkgs/niri.nix
+++ pkgs/niri.nix
@@ -1,3 +1,19 @@
-{ ... }: {
-  programs.niri.enable = true;
+# Niri with custom path for config.
+{ pkgs, ... }: {
+  programs.niri = {
+    enable = true;
+    package = pkgs.symlinkJoin {
+      name = "niri-custom";
+      paths = [ pkgs.niri ];
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        rm $out/bin/niri
+        makeWrapper ${pkgs.niri}/bin/niri $out/bin/niri \
+          --add-flags "-c /etc/nixos/shared/config/niri/niri.kdl"
+      '';
+    };
+  };
+  # 3. Prevent systemd from falling back to default environment paths.
+  systemd.user.services.niri.enableDefaultPath = false;
+}
 }
