2e543cd9cea3a22ee2c8ac187375d626d15d3b36
al1h3n <al1h3n@molni.com>
2026-06-14 22:57:08 +0500

    Fix

--- pkgs/niri.nix
+++ pkgs/niri.nix
@@ -2,15 +2,16 @@
 { pkgs, ... }: {
   programs.niri = {
     enable = true;
-    package = pkgs.niri.overrideAttrs (oldAttrs: {
-      nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
-      postInstall = (oldAttrs.postInstall or "") + ''
-        mv $out/bin/niri $out/bin/niri-unwrapped
-        makeWrapper $out/bin/niri-unwrapped $out/bin/niri \
-          --add-flags "-c /etc/nixos/shared/config/niri/niri.kdl"
-      '';
-    });
+    # package = pkgs.niri.overrideAttrs (oldAttrs: {
+    #   nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
+    #   postInstall = (oldAttrs.postInstall or "") + ''
+    #     mv $out/bin/niri $out/bin/niri-unwrapped
+    #     makeWrapper $out/bin/niri-unwrapped $out/bin/niri \
+    #       --add-flags "-c /etc/nixos/shared/config/niri/niri.kdl"
+    #   '';
+    # });
   };
   # 3. Prevent systemd from falling back to default environment paths.
   systemd.user.services.niri.enableDefaultPath = false;
+  environment.sessionVariables.NIRI_CONFIG = "/etc/nixos/shared/config/niri/niri.kdl";
 }
