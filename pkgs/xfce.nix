33c8217dd71a9c95700dfbe8adfb1eac041c1f7e
al1h3n <al1h3n@molni.com>
2026-06-19 18:36:35 +0500

    Fix

--- pkgs/xfce.nix
+++ pkgs/xfce.nix
@@ -4,11 +4,16 @@
     enableScreensaver = false;
     # noDesktop = true;
   };
-  environment.xfce.excludePackages = with pkgs.xfce; [
-    xfce4-taskmanager
-    xfce4-terminal
-    parole
-    tango-icon-theme
-    rodent-icon-theme
-  ];
+  environment = {
+    xfce.excludePackages = with pkgs.xfce; [
+      xfce4-taskmanager
+      xfce4-terminal
+      parole
+      tango-icon-theme
+      rodent-icon-theme
+    ];
+    systemPackages = with pkgs; [
+      xfce4-whiskermenu-plugin
+    ];
+  };
 }
