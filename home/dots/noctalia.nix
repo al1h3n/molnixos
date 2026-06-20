d77142ed60752bdb05e167cbf4966ff0f1f30aee
al1h3n <al1h3n@molni.com>
2026-06-20 22:05:58 +0500

    Fix

--- home/dots/noctalia.nix
+++ home/dots/noctalia.nix
@@ -2,7 +2,7 @@
   home.packages = with pkgs[
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default # noctalia-shell
   ];
-  xdg.dataFile."noctalia/settings.toml"= {
+  xdg.dataFile."noctalia/settings.toml" = {
     source = variables.noctalia5;
   };
 }
