1e14b09a4139ca1741e0485d6c3cb825e4f7541a
al1h3n <al1h3n@molni.com>
2026-07-15 15:38:34 +0500

    Fix?

--- flake.nix
+++ flake.nix
@@ -10,10 +10,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    home-manager-stable = {
-      url = "github:nix-community/home-manager/release-26.05";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
-    };
+    # home-manager-stable = {
+    #   url = "github:nix-community/home-manager/release-26.05";
+    #   inputs.nixpkgs.follows = "nixpkgs-stable";
+    # };
     home-manager-stable.follows = "home-manager";
 
     # CachyOS repository.
