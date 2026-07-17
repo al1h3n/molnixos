090b0d2faddc3d64368a5d1a3d70660dfbb602d2
al1h3n <al1h3n@molni.com>
2026-07-17 14:43:35 +0500

    Fix

--- home/dots/wezterm.nix
+++ home/dots/wezterm.nix
@@ -1,6 +1,8 @@
 { config, lib, variables, ... }: {
   xdg.configFile = {
-    "wezterm/wezterm.lua" = lib.mkForce { source = config.lib.file.mkOutOfStoreSymlink variables.wezterm; };
-    force = true;
+    "wezterm/wezterm.lua" = lib.mkForce {
+      source = config.lib.file.mkOutOfStoreSymlink variables.wezterm;
+      force = true;
+    };
   };
 }
