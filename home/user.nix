0fe97637dbf386019c517d649e235d4ee0d01778
al1h3n <al1h3n@molni.com>
2026-08-30 15:29:26 +0500

    Switch goofcord to dorion

--- home/user.nix
+++ home/user.nix
@@ -80,7 +80,7 @@
 
       # Social.
       _64gram # telegram-desktop
-      goofcord # Supports Vencord, Equicord and others. Optimized as well.
+      dorion # goofcord # Supports Vencord, Equicord and others. Optimized as well.
 
       # Notes.
       obsidian # appflowy # notion-app only on macOS, enhanced one is write screen.
